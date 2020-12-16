effdaa7278a2afc48126143ca1ebfed717f5347b
Andrii Tsvietkov <atsvietkov@student.ucode.world>
2020-12-16 13:27:11 +0200

    alternative game logic, monocolor complete

--- inc/level.h
+++ inc/level.h
@@ -35,5 +35,11 @@ void mx_delete_lvl(t_lvl **lvl);
 void mx_delete_lvls(t_lvl ***lvl);
 
 void gameplay(t_lvl **level, int map_size_y, int map_size_x);
+bool check_btn(t_lvl **level, int y, int x);
+bool check_box(t_lvl **level, int y, int x);
+void move_box(t_lvl **level, int y_now, int x_now, int y_step, int x_step);
+void hide_char(t_lvl **level, char c, int map_size_y, int map_size_x);
+
+
 
 void mx_print_lvl(t_lvl *lvl);  // TODO: Delete
--- src/gameplay.c
+++ src/gameplay.c
@@ -12,10 +12,14 @@ void gameplay(t_lvl **level, int map_size_y, int map_size_x) {
 
     (*level) -> map[y][x] = ' ';    // hide start position
 
-    int btn_counter = 0;
+    hide_char(level, MX_BTN_CHAR, map_size_y, map_size_x);
+    hide_char(level, MX_BOX_CHAR, map_size_y, map_size_x);
+
+    int btn_pushed = 0;
+    int btn_q = 0;
 
     for(int i = 0; (*level) -> btn_pos[i]; i++) {
-        btn_counter++;
+        btn_q++;
     }
 
     WINDOW *game_window = newwin(y_max, x_max, 2, 5);
@@ -28,100 +32,75 @@ void gameplay(t_lvl **level, int map_size_y, int map_size_x) {
                 waddch(game_window, (*level) -> map[i][j]);
             }
         }
-
-//        for (int i=0; (*level) -> btn_pos[i]; i++) {
-//            wmove(game_window, (*level) -> btn_pos[i][0], (*level) -> btn_pos[i][1]);
-//            waddch(game_window, MX_BTN_CHAR);
-//        }
-        
+        for (int i=0; (*level) -> btn_pos[i]; i++) {
+            wmove(game_window, (*level) -> btn_pos[i][0], (*level) -> btn_pos[i][1]);
+            waddch(game_window, MX_BTN_CHAR);
+        }
+        for (int i=0; (*level) -> box_pos[i]; i++) {
+            wmove(game_window, (*level) -> box_pos[i][0], (*level) -> box_pos[i][1]);
+            waddch(game_window, MX_BOX_CHAR);
+        }
         wmove(game_window, y, x);
         waddch(game_window, MX_PLAYER_CHAR);
 
         int ch = wgetch(game_window);
-        if (ch == KEY_DOWN) {
-            if(y+1 < y_max) {
-                if ((*level) -> map[y+1][x] == ' ' || (*level) -> map[y+1][x] == MX_BTN_CHAR) {
-                    y++;
-                }
 
-                else if((*level) -> map[y+1][x] == MX_BOX_CHAR) {
-                    if((*level) -> map[y+2][x] == ' ' && y+2 < y_max) {
-                        (*level) -> map[y+2][x] = MX_BOX_CHAR;
-                        (*level) -> map[y+1][x] = ' ';
-                        y++;
+        if(ch == KEY_DOWN) {
+            if(y+1 < y_max) {
+                if ((*level) -> map[y+1][x] != '#') {
+                    if (check_box(level, y+1, x)) {
+                        if((*level) -> map[y+2][x] != '#' && !(check_box(level, y+2, x))) {
+                            move_box(level,y+1,x,1,0);
+                            y++;
+                        }
                     }
-                    else if((*level) -> map[y+2][x] == MX_BTN_CHAR && y+2 < y_max) {
-                        (*level) -> map[y+2][x] = 'x';
-                        (*level) -> map[y+1][x] = ' ';
-                        btn_counter--;
+                    else {
                         y++;
                     }
                 }
-                // else if((*level) -> map[y+1][x] == MX_BOX_CHAR) {
-                //     if(((*level) -> map[y+2][x] == ' ' || (*level) -> map[y+2][x] == MX_BTN_CHAR) && y+2 < y_max) {
-                //         (*level) -> map[y+2][x] = MX_BOX_CHAR;
-                //         (*level) -> map[y+1][x] = ' ';
-                //         y++;
-                //     }
-                // }
             }
         }
-        if (ch == KEY_UP){
-            if(y > 0) {
-                if ((*level) -> map[y-1][x] == ' ' || (*level) -> map[y-1][x] == MX_BTN_CHAR) {
-                    y--;
-                }
-                else if((*level) -> map[y-1][x] == MX_BOX_CHAR) {
-                    if((*level) -> map[y-2][x] == ' ' && y-2 > 0) {
-                        (*level) -> map[y-2][x] = MX_BOX_CHAR;
-                        (*level) -> map[y-1][x] = ' ';
-                        y--;
+        if(ch == KEY_UP) {
+            if(y-1 > 0) {
+                if ((*level) -> map[y-1][x] != '#') {
+                    if (check_box(level, y-1, x)) {
+                        if((*level) -> map[y-2][x] != '#' && !(check_box(level, y-2, x))) {
+                            move_box(level,y-1,x,-1,0);
+                            y--;
+                        }
                     }
-                    else if((*level) -> map[y-2][x] == MX_BTN_CHAR && y-2 > 0) {
-                        (*level) -> map[y-2][x] = 'x';
-                        (*level) -> map[y-1][x] = ' ';
-                        btn_counter--;
+                    else {
                         y--;
                     }
                 }
             }
         }
-        if (ch == KEY_LEFT) {
-            if(x > 0) {
-                if ((*level) -> map[y][x-1] == ' ' || (*level) -> map[y][x-1] == MX_BTN_CHAR) {
-                    x--;
-                }
-                else if((*level) -> map[y][x-1] == MX_BOX_CHAR) {
-                    if((*level) -> map[y][x-2] == ' ' && x-2 > 0) {
-                        (*level) -> map[y][x-2] = MX_BOX_CHAR;
-                        (*level) -> map[y][x-1] = ' ';
-                        x--;
-                    }
-                    else if((*level) -> map[y][x-2] == MX_BTN_CHAR && x-2 > 0) {
-                        (*level) -> map[y][x-2] = 'x';
-                        (*level) -> map[y][x-1] = ' ';
-                        btn_counter--;
-                        x--;
-                    }
-                }
-            }
-        }
-        if (ch == KEY_RIGHT) {
+        if(ch == KEY_RIGHT) {
             if(x+1 < x_max) {
-                if ((*level) -> map[y][x+1] == ' ' || (*level) -> map[y][x+1] == MX_BTN_CHAR) {
-                    x++;
-                }
-                else if((*level) -> map[y][x+1] == MX_BOX_CHAR) {
-                    if((*level) -> map[y][x+2] == ' ' && x+2 < x_max) {
-                        (*level) -> map[y][x+2] = MX_BOX_CHAR;
-                        (*level) -> map[y][x+1] = ' ';
+                if ((*level) -> map[y][x+1] != '#') {
+                    if (check_box(level, y, x+1)) {
+                        if((*level) -> map[y][x+2] != '#' && !(check_box(level, y, x+2))) {
+                            move_box(level,y,x+1,0,1);
+                            x++;
+                        }
+                    }
+                    else {
                         x++;
                     }
-                    else if((*level) -> map[y][x+2] == MX_BTN_CHAR && x+2 < x_max) {
-                        (*level) -> map[y][x+2] = 'x';
-                        (*level) -> map[y][x+1] = ' ';
-                        btn_counter--;
-                        x++;
+                }
+            }
+        }
+        if(ch == KEY_LEFT) {
+            if(x-1 > 0) {
+                if ((*level) -> map[y][x-1] != '#') {
+                    if (check_box(level, y, x-1)) {
+                        if((*level) -> map[y][x-2] != '#' && !(check_box(level, y, x-2))) {
+                            move_box(level,y,x-1,0,-1);
+                            x--;
+                        }
+                    }
+                    else {
+                        x--;
                     }
                 }
             }
@@ -129,9 +108,54 @@ void gameplay(t_lvl **level, int map_size_y, int map_size_x) {
         if (ch == 'q') {
             break;
         }
-        if (btn_counter == 0) {
-            break;
+        btn_pushed = 0;
+        for(int i = 0; (*level) -> btn_pos[i]; i++) {
+            if(check_box(level, (*level) -> btn_pos[i][0], (*level) -> btn_pos[i][1])) {
+                btn_pushed++;
+            }
+        }
+
+        if (btn_q == btn_pushed) {
+             break;
         }
     }
     delwin(game_window);
+}
+
+bool check_btn(t_lvl **level, int y, int x) {
+    for (int i = 0; (*level) -> btn_pos[i]; i++) {
+        if ((*level) -> btn_pos[i][0] == y && (*level) -> btn_pos[i][1] == x) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+bool check_box(t_lvl **level, int y, int x) {
+    for (int i = 0; (*level) -> box_pos[i]; i++) {
+        if ((*level) -> box_pos[i][0] == y && (*level) -> box_pos[i][1] == x) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void move_box(t_lvl **level, int y_now, int x_now, int y_step, int x_step) {
+    for (int i = 0; (*level) -> box_pos[i]; i++) {
+        if ((*level) -> box_pos[i][0] == y_now && (*level) -> box_pos[i][1] == x_now) {
+            (*level) -> box_pos[i][0] = y_now + y_step;
+            (*level) -> box_pos[i][1] = x_now + x_step;
+            break;
+        }
+    }
+}
+
+void hide_char(t_lvl **level, char c, int map_size_y, int map_size_x) {
+    for(int i=0; i<map_size_y; i++) {
+        for(int j=0; j<map_size_x; j++) {
+            if ((*level) -> map[i][j] == c) {
+                (*level) -> map[i][j] = ' ';
+            }
+        }
+    }
 }
